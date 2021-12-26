800dcc8024700139174cc74b0c3f269a6933fe7c
Coder962 <76476530+Coder962@users.noreply.github.com>
2021-12-26 18:30:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function setup() {
 }
 img = "";
 status = "";
+objects = [];
 function preload() {
     img = loadImage('dog_cat.jpg');
 }
@@ -19,17 +20,36 @@ function gotResult(error , results) {
         console.log("error");
     }
     console.log(results);
+    objects = results;
 }
 function draw() {
     image(img,0,0,800,450);
-    fill("#FF0000");
-    text("Dog",45,75);
-    noFill();
-    stroke("#FF0000");
-    rect(30,60,450,350);
-    fill("#FF0000");
-    text("Cat",320,120);
-    noFill();
-    stroke("#FF0000");
-    rect(300,80,270,320);
+    
+
+    if(status != ""){
+      for (var i = 0; i < objects.length; i++) {
+        document.getElementById("status").innerHTML = "Status : Object Detected";
+  
+        fill(255, 0, 0);
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke(255, 0, 0);
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }
+        // objects[0].label;
+        // objects[0].width;
+
+    }
+    // fill("#FF0000");
+    // text("Dog",45,75);
+    // noFill();
+    // stroke("#FF0000");
+    // rect(30,60,450,350);
+
+    // fill("#FF0000");
+    // text("Cat",320,120);
+    // noFill();
+    // stroke("#FF0000");
+    // rect(300,80,270,320);
 }
